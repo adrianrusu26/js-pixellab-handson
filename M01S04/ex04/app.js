8f290d390b3fe450c0acfc07a30d5bbc7d2efa2f
adrianrusu26 <adrian.rusu176@gmail.com>
2022-05-31 22:22:02 +0300

    curs jS 31.05.2022

---
+++ M01S04/ex04/app.js
@@ -0,0 +1,41 @@
+var i = 0;
+
+// for (i = 0; i <= 3; i++) {
+//   console.log(i);
+// }
+
+// for (i = 1; i <= 100; i++) {
+//   console.log(i);
+// }
+
+// for (i = 1; i < 100; i++) {
+//   console.log(i);
+// }
+
+// for (i = 1; i < 100; i++) {
+//   console.log(i);
+// }
+
+///
+
+// for (i = 49; i < 100; i++) {
+//   console.log(i);
+
+//   if (i === 50) {
+//     break;
+//   }
+// }
+
+// for (i = 1; i <= 100; i++) {
+//   if (i % 2 !== 0) {
+//     continue;
+//   }
+
+//   console.log(i);
+// }
+
+for (i = 1; i <= 100; i++) {
+  if (i % 5 === 0) {
+    console.log(i);
+  }
+}
---
+++ M01S04/ex06/app.js
@@ -0,0 +1,35 @@
+var person = {
+  name: 'Adrian',
+  surname: 'Rusu',
+  age: 28,
+  petOwner: false,
+  skills: [
+    'html',
+    'javascript',
+    'css',
+    'java',
+    'c++',
+    'node',
+    'jquery',
+    'node.js',
+  ],
+  friends: [
+    {
+      name: 'Larry',
+      surname: 'Larryson',
+      age: 30,
+    },
+
+    {
+      name: 'Steven',
+      surname: 'Stevenson',
+      age: 31,
+    },
+
+    {
+      name: 'Carol',
+      surname: 'Carolson',
+      age: 29,
+    },
+  ],
+};
